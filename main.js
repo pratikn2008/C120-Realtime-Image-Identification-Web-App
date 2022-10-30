9af0137f36263136ff112f7da7e65168cda320fa
Pratik Nayak <104261131+pratikn2008@users.noreply.github.com>
2022-10-30 11:48:41 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -3,4 +3,33 @@ function setup() {
   canvas.center();
   video = createCapture(VIDEO);
   video.hide();
+  classifier = ml5.imageClassifier('MobileNet' ,modelLoaded);
+}
+
+function modelLoaded(){
+  console.log("Model Loaded!");
+}
+
+function draw(){
+image(video, 0, 0, 300, 250);
+classifier.classify(video, gotResult);
+}
+
+function gotResult(error, results) {
+if(error) {
+  console.error(error);
+}
+else{
+  if((results[0].confidence > 0.5) && (previous_result !=results[0].label)){
+    previous_result = results[0].label;
+    var synth = window.speechSynthesis;
+    speak_data = 'Object detected is - '+results[0].label;
+    var utterThis = new SpeechSynthesisUtterance(speak_data);
+    synth.speak(utterThis);
+
+    document.getElementById("results_object_name").innerHTML = results[0].label;
+
+    document.getElementById("results_object_accuracy").innerHTML = results[0].confidence.toFixed(3);
+    }
+  }
 }
